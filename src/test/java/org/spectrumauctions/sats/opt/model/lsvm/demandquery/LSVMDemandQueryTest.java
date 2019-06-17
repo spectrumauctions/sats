7b518a455db37422eac8731b907e46772ec9f244
fabio <islerfab@gmail.com>
2019-06-17 15:17:23 +0200

    feat(lsvm): using solution pool mode 3 for lsvm demand query

--- src/test/java/org/spectrumauctions/sats/opt/model/lsvm/demandquery/LSVMDemandQueryTest.java
+++ src/test/java/org/spectrumauctions/sats/opt/model/lsvm/demandquery/LSVMDemandQueryTest.java
@@ -1,13 +1,14 @@
 package org.spectrumauctions.sats.opt.model.lsvm.demandquery;
 
+import com.google.common.collect.Lists;
+import edu.harvard.econcs.jopt.solver.SolveParam;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
-import org.spectrumauctions.sats.core.model.lsvm.LSVMBidder;
-import org.spectrumauctions.sats.core.model.lsvm.LSVMLicense;
-import org.spectrumauctions.sats.core.model.lsvm.LSVMWorld;
-import org.spectrumauctions.sats.core.model.lsvm.LocalSynergyValueModel;
+import org.spectrumauctions.sats.core.model.lsvm.*;
+import org.spectrumauctions.sats.core.util.random.DoubleInterval;
+import org.spectrumauctions.sats.core.util.random.IntegerInterval;
 import org.spectrumauctions.sats.core.util.random.JavaUtilRNGSupplier;
 
 import java.math.BigDecimal;
@@ -27,13 +28,42 @@ public class LSVMDemandQueryTest {
         List<LSVMBidder> bidders = new LocalSynergyValueModel().createNewPopulation(new JavaUtilRNGSupplier(73246104));
         LSVMWorld world = bidders.iterator().next().getWorld();
         Map<LSVMLicense, BigDecimal> prices = new HashMap<>();
-        world.getLicenses().forEach(license -> prices.put(license, BigDecimal.valueOf(0)));
+        world.getLicenses().forEach(license -> prices.put(license, BigDecimal.TEN));
 
         for (LSVMBidder bidder : bidders) {
             LSVM_DemandQueryMIP mip = new LSVM_DemandQueryMIP(bidder, prices);
-            LSVM_DemandQueryMipResult result = mip.getResult();
-            Assert.assertTrue(result.getResultingBundle().value().doubleValue() > 0);
-            logger.info(result.getResultingBundle());
+            List<LSVM_DemandQueryMipResult> results = mip.getResultPool(10);
+            Assert.assertEquals(10, results.size());
+            results.forEach(result -> Assert.assertTrue(result.getResultingBundle().value().doubleValue() > 0));
+            logger.info(results);
         }
+    }
+
+    @Test
+    public void testNationalBidder() {
+        JavaUtilRNGSupplier rng = new JavaUtilRNGSupplier(73246104);
+        LSVMWorldSetup.LSVMWorldSetupBuilder worldSetupBuilder = new LSVMWorldSetup.LSVMWorldSetupBuilder();
+        worldSetupBuilder.setNumberOfColumnsInterval(new IntegerInterval(1));
+        worldSetupBuilder.setNumberOfRowsInterval(new IntegerInterval(6));
+
+        LSVMWorld world = new LSVMWorld(worldSetupBuilder.build(), rng);
+
+        LSVMBidderSetup.NationalBidderBuilder nationalBidderBuilder = new LSVMBidderSetup.NationalBidderBuilder();
+        nationalBidderBuilder.setValueInterval(new DoubleInterval(4));
+
+        List<LSVMBidder> bidders = world.createPopulation(Lists.newArrayList(nationalBidderBuilder.build()), rng);
+        Map<LSVMLicense, BigDecimal> prices = new HashMap<>();
+        world.getLicenses().forEach(license -> prices.put(license, BigDecimal.valueOf(3)));
+
+        Assert.assertEquals(1, bidders.size());
+
+        LSVMBidder bidder = bidders.get(0);
+
+        LSVM_DemandQueryMIP mip = new LSVM_DemandQueryMIP(bidder, prices);
+
+        logger.info("#Vars: \t{}", mip.getMip().getMip().getNumVars());
+        logger.info("#Constraints: \t{}", mip.getMip().getMip().getNumConstraints());
+        List<LSVM_DemandQueryMipResult> result = mip.getResultPool(10);
+        Assert.assertEquals(10, result.size());
     }
 }
