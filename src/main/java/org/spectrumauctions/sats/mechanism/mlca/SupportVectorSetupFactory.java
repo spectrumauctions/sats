4764e53ee6439f855fa9855857a892574a7a7cfe
Manuel Beyeler <manuel@beyeler.xyz>
2021-06-08 22:05:57 +0200

    new hyperparameters for MLCA -> full MLCA run for paper

--- src/main/java/org/spectrumauctions/sats/mechanism/mlca/SupportVectorSetupFactory.java
+++ src/main/java/org/spectrumauctions/sats/mechanism/mlca/SupportVectorSetupFactory.java
@@ -17,34 +17,34 @@ public enum SupportVectorSetupFactory {
 	
 	private Map<KernelType,Kernel> gsvmDefaultKernels = Map.of(
 			KernelType.Linear, new KernelLinear(0d, 0.2d), 
-			KernelType.Quadratic, new KernelQuadratic(0d, 1d, 0025d),
+			KernelType.Quadratic, new KernelQuadratic(0d, 1d, 0.0078125d),
 			KernelType.Polynomial, new KernelDotProductPolynomial(new double[] {0d,1d,0025d}),
 			KernelType.Exponential, new KernelDotProductExponential(10d, 10d),
 			KernelType.Gaussian, new KernelGaussian(20d, 10d));
 	
 	private Map<KernelType,Kernel> lsvmDefaultKernels = Map.of(
 			KernelType.Linear, new KernelLinear(0d,80d),
-			KernelType.Quadratic, new KernelQuadratic(0d, 80d, 0.5d),
+			KernelType.Quadratic, new KernelQuadratic(0d, 1d, 0.03125d),
 			KernelType.Polynomial, new KernelDotProductPolynomial(new double[] {0d,80d,0.5d}),
 			KernelType.Exponential, new KernelDotProductExponential(5d, 10d),
 			KernelType.Gaussian, new KernelGaussian(10d,10d));
 	
 	private Map<KernelType,Kernel> mrvmDefaultKernels = Map.of(
-			KernelType.Linear, new KernelLinear(0d,100d),
-			KernelType.Quadratic, new KernelQuadratic(0d,100d,0.1d),
+			KernelType.Linear, new KernelLinear(0d,1d),
+			KernelType.Quadratic, new KernelQuadratic(0d,1d,0.001d),
 			KernelType.Polynomial, new KernelDotProductPolynomial(new double[] {0d,100d,0.1d}),
 			KernelType.Exponential, new KernelDotProductExponential(100d, 100d),
 			KernelType.Gaussian, new KernelGaussian(100d, 100d));
 	
 	public SupportVectorSetup createDefaultGSVMSupportVectorSetup(KernelType kernelType) {
-		return new SupportVectorSetup(3000d, 0.05d, BigDecimal.ONE,true,1e-9, this.gsvmDefaultKernels.get(kernelType));
+		return new SupportVectorSetup(1000000d, 0.00001d, BigDecimal.valueOf(100),true,1e-8, this.gsvmDefaultKernels.get(kernelType));
 	}
 	
 	public SupportVectorSetup createDefaultLSVMSupportVectorSetup(KernelType kernelType) {
-		return new SupportVectorSetup(100d,0.0001d,BigDecimal.ONE,true,1e-9,this.lsvmDefaultKernels.get(kernelType));
+		return new SupportVectorSetup(100000d,0.00001d,BigDecimal.valueOf(100),true,1e-8,this.lsvmDefaultKernels.get(kernelType));
 	}
 	
 	public SupportVectorSetup createDefaultMRVMSupportVectorSetup(KernelType kernelType) {
-		return new SupportVectorSetup(100d,0.0001d,BigDecimal.ONE.divide(BigDecimal.valueOf(100000)),true,1e-9,this.mrvmDefaultKernels.get(kernelType));
+		return new SupportVectorSetup(1000d,0.00001d,BigDecimal.ONE.divide(BigDecimal.valueOf(1000000)),true,1e-8,this.mrvmDefaultKernels.get(kernelType));
 	}
 }
